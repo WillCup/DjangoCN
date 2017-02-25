68fa73dc3d2aa6b0835d597a1b03a6c724fa8e25
river <zhoucc2012@gmail.com>
2017-02-25 12:33:35 +0800

    完善评论系统的数据库设计

--- Comment/models.py
+++ Comment/models.py
@@ -1,20 +1,59 @@
 from django.db import models
 from Usercenter.models import User
+from Blog.models import BlogPost
+from Collect.models import Collect
+from FAQuestion.models import FAQuestion
 
-# Create your models here.
+
+class BlogPostManager(models.Manager):
+    def get_queryset(self):
+        return super(BlogPostManager, self).get_queryset().filter(obj_type='blog')
+
+
+class UserManager(models.Manager):
+    def get_queryset(self):
+        return super(UserManager, self).get_queryset().filter(obj_type='user')
+
+
+class CollectManager(models.Manager):
+    def get_queryset(self):
+        return super(CollectManager, self).get_queryset().filter(obj_type='collect')
+
+
+class FAQuestionManager(models.Manager):
+    def get_queryset(self):
+        return super(FAQuestionManager, self).get_queryset().filter(obj_type='question')
+
 
 class Comment(models.Model):
-    COMMENT_STATUS = (
-        ('deleted','已删除'),
-        ('active','正常'),
+    """
+    dynamic create record for objs.  Just for design.
+    """
+    STATUS_CHOICES = (
+        ('deleted', '已删除'),
+        ('active', '正常'),
     )
-    obj_type = models.CharField(max_length=10, verbose_name='评论对象')
-    obj_id = models.IntegerField(null=True, verbose_name='评论对象ID')
+    OBJ_CHOICES = (
+        ('user', User),
+        ('blog', BlogPost),
+        ('collect', Collect),
+        ('question', FAQuestion)
+    )
+    obj_type = models.CharField(max_length=10, choices=OBJ_CHOICES, default='blog', verbose_name='评论对象')
+    obj_id = models.IntegerField(null=True, verbose_name='评论对象ID')  # need recode.
     user_pk = models.ForeignKey(User, related_name='commented', verbose_name='被评论人')
     reply_user = models.ForeignKey(User, related_name='comments', verbose_name='我的评论')
     reply_content = models.TextField(verbose_name='评论内容')
     published = models.DateTimeField(auto_now_add=True, verbose_name='评论时间')
-    is_active = models.CharField(max_length=10, choices=COMMENT_STATUS, default='active', verbose_name='评论状态')
+    is_active = models.CharField(max_length=10, choices=STATUS_CHOICES, default='active', verbose_name='评论状态')
+
+    objects = models.Manager()
+    # set custom Manager
+    user = UserManager()
+    blog = BlogPostManager()
+    collect = CollectManager()
+    question = FAQuestionManager()
+
     class Meta:
         verbose_name = '评论'
         verbose_name_plural = '评论'
